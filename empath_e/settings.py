04d5d380016c216673206667c199266d66163944
Clémence Mourot <clem.mourot@hotmail.fr>
2020-01-15 17:57:28 +0100

    Static files collecting set up

--- empath_e/settings.py
+++ empath_e/settings.py
@@ -122,6 +122,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
 
 LOGIN_URL = '/login/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "app/static"),
+]
+
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
--- empath_e/urls.py
+++ empath_e/urls.py
@@ -13,9 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls import include, url
+from django.conf import settings
+from django.conf.urls.static import static
 
 from app.views.base import index
 
@@ -23,4 +27,4 @@ urlpatterns = [
     url(r'^$', index),
     url(r'^', include('app.urls', namespace='app')),
     url(r'^admin/', admin.site.urls),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
