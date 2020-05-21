637f0af115579d7d87f2323354559c8f46278e46
Clémence Mourot <clem.mourot@hotmail.fr>
2020-05-21 16:09:52 +0200

    Username and time displayed on chat

--- chat/consumers.py
+++ chat/consumers.py
@@ -23,24 +23,33 @@ class ChatConsumer(WebsocketConsumer):
         )
 
     # Receive message from WebSocket
+    
     def receive(self, text_data):
+
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
+
+        response = {
+            'message': message,
+            'username': self.scope['user'].username,
+            }
 
         # Send message to room group
         async_to_sync(self.channel_layer.group_send)(
             self.room_group_name,
             {
                 'type': 'chat_message',
-                'message': message
+                'response': response
             }
         )
 
     # Receive message from room group
     def chat_message(self, event):
-        message = event['message']
+        message = event['response']['message']
+        user = event['response']['username']
 
         # Send message to WebSocket
         self.send(text_data=json.dumps({
-            'message': message
+            'message': message,
+            'user': user
         }))
--- chat/views.py
+++ chat/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
 
-# @login_required
+@login_required
 def room(request, room_name):
     return render(request, 'chat/room.html', {
         'room_name': room_name
