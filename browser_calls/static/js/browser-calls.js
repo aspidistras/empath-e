9846eb049900f4869207cda89863799c3e2f83fd
Clémence Mourot <clem.mourot@hotmail.fr>
2020-06-04 19:26:38 +0200

    Chat style optimized
    + browser calls JS bugs fixed

--- browser_calls/static/js/browser-calls.js
+++ browser_calls/static/js/browser-calls.js
@@ -1,73 +1,71 @@
-// Store some selectors for elements we'll reuse
-var callStatus = $("#call-status");
-var answerButton = $(".answer-button");
-var callButton = $(".call-button");
-var hangUpButton = $(".hangup-button");
-
 /* Helper function to update the call status bar */
 function updateCallStatus(status) {
-    callStatus.attr('placeholder', status);
+    $("#call-status").attr('placeholder', status);
 }
 
 /* Get a Twilio Client token with an AJAX request */
-$(document).ready(function() {
+function setup() {
     $.get("/contact/token", {forPage: window.location.pathname}, function(data) {
-        // Set up the Twilio Client Device with the token
-        Twilio.Device.setup(data.token);
+
+         // Set up the Twilio Client Device with the token
+        Twilio.Device.setup(data.token, {debug: true});
     });
-});
+
+}
 
 /* Callback to let us know Twilio Client is ready */
-Twilio.Device.ready(function (device) {
-    updateCallStatus("Ready");
+Twilio.Device.on("ready", function (device) {
+    updateCallStatus("Prêt pour l'appel");
 });
 
 /* Report any errors to the call status display */
-Twilio.Device.error(function (error) {
-    updateCallStatus("ERROR: " + error.message);
+Twilio.Device.on("error", function (error) {
+    console.log("ERROR: " + error.message);
 });
 
 /* Callback for when Twilio Client initiates a new connection */
-Twilio.Device.connect(function (connection) {
+Twilio.Device.on("connect", function (connection) {
+    alert("cc");
     // Enable the hang up button and disable the call buttons
-    hangUpButton.prop("disabled", false);
-    callButton.prop("disabled", true);
-    answerButton.prop("disabled", true);
+    $(".hangup-button").prop("disabled", false);
+    $(".call-button").prop("disabled", true);
+    $(".answer-button").prop("disabled", true);
 
     updateCallStatus("Appel en cours");
 });
 
 /* Callback for when a call ends */
-Twilio.Device.disconnect(function(connection) {
+Twilio.Device.on("disconnect", function(connection) {
     // Disable the hangup button and enable the call buttons
-    hangUpButton.prop("disabled", true);
-    callButton.prop("disabled", false);
+    $(".hangup-button").prop("disabled", true);
+    $(".call-button").prop("disabled", false);
 
     updateCallStatus("Appel terminé");
 });
 
 /* Callback for when Twilio Client receives a new incoming call */
-Twilio.Device.incoming(function(connection) {
+Twilio.Device.on("incoming", function (connection) {
     updateCallStatus("Appel en attente");
-
+    alert("incoming");
     // Set a callback to be executed when the connection is accepted
     connection.accept(function() {
         updateCallStatus("Appel en cours");
     });
 
+    $(".answer-button").prop("disabled", false);
     // Set a callback on the answer button and enable it
-    answerButton.click(function() {
+    $(".answer-button").click(function() {
         connection.accept();
     });
-    answerButton.prop("disabled", false);
 });
 
 /* Call the user from the requests page */
 function callUser() {
     updateCallStatus("Appel...");
-
+    console.log("callUSer");
     // Our backend will assume that no params means a call to support_agent
     Twilio.Device.connect();
+    alert("call user end");
 }
 
 /* End a call */
